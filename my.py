92646c9e15da776bf0c8889799e4b655c728a5e2
Alex Matiyaka <matiacka@yandex.ru>
2023-03-29 16:41:46 +0300

    add 4 solutions

---
+++ my.py
@@ -0,0 +1,8 @@
+# n = list(map(int, input().strip().split()))    # n = [int(i) for i in input().split()]
+# numbers = [int(input()) for _ in range(int(input()))]
+# numbers = [i + j for i in ['a', 'b'] for j in ['z', 'h', 'q']]
+# объявление функции
+# объявление функции
+# объявление функции
+
+
